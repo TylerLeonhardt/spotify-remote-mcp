040a3f0163987f25f0e08871ba12f69f0103d974
TylerLeonhardt <2644648+TylerLeonhardt@users.noreply.github.com>
2025-07-30 21:43:34 -0700

    Remove tool for API that no longer exists

--- src/tools.contribution.ts
+++ src/tools.contribution.ts
@@ -1,6 +1,5 @@
 import './tools/whoami.js';
 import './tools/search.js';
-import './tools/getRecommendations.js';
 import './tools/playSongs.js';
 import './tools/listDevices.js';
 import './tools/getPlaybackState.js';
